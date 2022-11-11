e731419980eab14655b4631296b756041eeaa75a
Victoria <tikhomirovavs@gmail.com>
2022-11-11 18:48:58 +0300

    add example4

---
+++ example4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите Ваше имя ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("УРА, это же Маша");
+}
+
+else {
+    Console.WriteLine("Привет, " + username);
+}
